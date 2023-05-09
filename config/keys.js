163fc9393ff7deec22f3250476993878d0b2a38a
Rahul Pareek <41017269+bisomaticc@users.noreply.github.com>
2023-05-09 12:40:54 +0530

    Adding files

---
+++ config/keys.js
@@ -0,0 +1,5 @@
+dbPassword = 'mongodb+srv://bisomaticc:'+ encodeURIComponent('9352703544') + '@cluster0.kvuptna.mongodb.net/?retryWrites=true';
+// mar
+module.exports = {
+    mongoURI: dbPassword
+};
